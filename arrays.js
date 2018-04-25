0ca78b055c4c5b121e7442456d417c3c707be4bc
Ariel Zhen <zhenpingping@live.com>
2018-04-25 03:07:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,20 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array,ele){
+  return [ele..., array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array,ele){
+  array.unshift(ele);
+  return array;
+}
+
+function addElementToEndOfArray(array,ele){
+  return [...array, ele];
+}
+
+function destructivelyAddElementToEndOfArray(array,ele){
+  array.push(ele);
+  return array;
+}
+
